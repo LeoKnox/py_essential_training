7fbc6517fb2f65bb752a30df984306308b54dbd3
LeoKnox <LeoKnox@users.noreply.github.com>
2020-04-15 20:13:49 -0700

    Add files via upload

--- types.py
+++ types.py
@@ -4,5 +4,13 @@ z = 7.0
 s = '7.0'
 b = True
 
-print ('x is {}'.format(x))
-print(type(x))
+t = 'seven'.capitalize()
+u = ''' 
+-------
+seven
+-------
+'''
+
+print ('t is {1} and {0}'.format(t, u))
+print ('numbers is {1:<09} and {0:>08}'.format(y, z))
+print(type(t))
