0ed1d4426a099e0fe5ce76354889bf9bc9ca9376
LeoKnox <LeoKnox@users.noreply.github.com>
2020-04-16 12:18:46 -0700

    Add files via upload

---
+++ sequence.py
@@ -0,0 +1,9 @@
+#x = [ 1, 2, 3, 4, 5] #list
+#x = range(3, 15, 2)
+y = list(range(6))
+x = { 'one': 1, 'two': 2, 'three':3 }
+
+#x[4] = 99
+
+for k, v in x.items():
+    print ('{} is {}'.format(k, v))
