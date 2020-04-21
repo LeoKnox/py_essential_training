14bcea4e208f215508c93134deb332cc72cb2165
LeoKnox <LeoKnox@users.noreply.github.com>
2020-04-20 19:10:21 -0700

    Add files via upload

--- tree.py
+++ tree.py
@@ -1,59 +1,21 @@
-class Tree:
+class node:
     def __init__(self, info):
+        self.info = info
         self.left = None
         self.right = None
-        self.info = info
-    
-    def delNode(self, info):
-        if self== None:
-            return False
-        if self.info == info:
-            if self.left.info == info and self.right.info == None:
-                self = None
-                return True
-            elif self.left and self.right == None:
-                self = self.left
-                return True
-            elif self.left == None and self.right:
-                self = self.right
-                return True
 
-            self.left = None
+class Tree:
+    def __init__(self):
+        self.head = None
     
     def addNode(self, info):
-        if info < self.info:
-            if self.left:
-                return self.left.addNode(info)
-            else:
-                self.left = Tree(info)
-        else:
-            if self.right:
-                return self.right.addNode(info)
-            else:
-                self.right = Tree(info)
-    
-    def traceTree(self):
-        if self.left:
-            self.left.traceTree()
-        print( self.info),
-        if self.right:
-            self.right.traceTree()
-    
-    def delTree(self):
-        self == None
+        newNode = node(info)
+        if self.head == None:
+            self.head = newNode
+        elif info < self.head.info:
+            self.head.left = newNode
 
-t = Tree(7)
+t = Tree()
+t.addNode(6)
 t.addNode(3)
-t.addNode(10)
-t.addNode(99)
-t.addNode(23)
-t.addNode(4)
-t.addNode(1)
-t.addNode(2)
-t.addNode(77)
-t.addNode(77)
-t.addNode(76)
-t.addNode(3)
-t.addNode(43)
-t.addNode(9)
-t.traceTree()
+print(t.head.left.info)
