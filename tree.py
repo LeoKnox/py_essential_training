dc1cb0a35efbd29ee884ba27607b1d43e1b4841e
LeoKnox <LeoKnox@users.noreply.github.com>
2020-04-20 22:43:36 -0700

    Add files via upload

--- tree.py
+++ tree.py
@@ -7,15 +7,24 @@ class node:
 class Tree:
     def __init__(self):
         self.head = None
+
+    
     
     def addNode(self, info):
         newNode = node(info)
+        temp = self.head
         if self.head == None:
             self.head = newNode
-        elif info < self.head.info:
-            self.head.left = newNode
+        elif info < temp.info:
+            if temp.left == None:
+                print(self.head.info)
+                self.head.left == newNode
+            else:
+                temp = temp.left()
 
 t = Tree()
 t.addNode(6)
 t.addNode(3)
-print(t.head.left.info)
+t.addNode(1)
+n = ()
+print(t.head.left)
