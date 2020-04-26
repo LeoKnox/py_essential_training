7ef42d04040a0321d4dff2dba4a9391843ce88a1
LeoKnox <LeoKnox@users.noreply.github.com>
2020-04-25 22:59:39 -0700

    Add files via upload

---
+++ hello3.py
@@ -0,0 +1,8 @@
+print('Elric and Stormbringer'.casefold()) #more aggressive then lower will work with foreign characters
+
+s1 = 'Conan the Barbarian'
+s2 = 'Iron and Steel'
+s3 = 'Lord of the ring ' 'Fellowship of the ring'
+
+print (s1 + ' ' + s2)
+print (s3)
