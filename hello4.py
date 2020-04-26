9819fb00802de259ffa59ace64cdb5f341d2dd9a
LeoKnox <LeoKnox@users.noreply.github.com>
2020-04-26 03:07:07 -0700

    Add files via upload

---
+++ hello4.py
@@ -0,0 +1,10 @@
+print ('We have a Hulk')
+
+x = 99
+y = 44
+z = 80 * 347 *34243
+print('the number is {xx} and {bb}'.format(bb = x, xx = y))
+print('the number is {1:+05} and {0:<05} {1:>5}'.format(x, y))
+print('with commas: {:,}'.format(z).replace(',','.'))
+print('number: {:.3f}'.format(z))
+print(f'binary {z:b} {x:o}')
