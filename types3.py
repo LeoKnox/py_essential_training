c77efefcf2c00c6cda85b6e52c59b305d1edac44
LeoKnox <LeoKnox@users.noreply.github.com>
2020-04-28 17:10:47 -0700

    Add files via upload

---
+++ types3.py
@@ -0,0 +1,11 @@
+x = '47'
+y = float(x)
+z = abs(int(x))
+m = divmod(int(x), 3)
+n = complex(int(x), 3)
+
+print(f'm is type{type(m)} and is {m}')
+print(f'x is {type(x)}')
+print(f'x is {x}')
+print(f'y is {type(y)}')
+print(f'y is {y}')
